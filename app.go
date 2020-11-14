75f027ddc00ddf3ac5c4fc6a7144b2febd901482
Roman Luitko <dossalab@pm.me>
2020-11-14 13:54:25 +0300

    bye-bye c, let's rewrite it in go

---
+++ app.go
@@ -0,0 +1,43 @@
+/*
+ * Part of the Conic Palace - robot control center
+ *
+ * See LICENSE.md for licensing information
+ * Roman Luitko, 2020
+ */
+
+package main
+
+import (
+    "github.com/gotk3/gotk3/gtk"
+    "log"
+)
+
+func createLayout(window *gtk.Window) error {
+	l, err := gtk.LabelNew("Hello, gotk3!")
+	if err != nil {
+		return err
+	}
+
+	window.Add(l)
+	return nil
+}
+
+func main() {
+	gtk.Init(nil)
+
+	win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
+	if err != nil {
+		log.Fatal("Unable to create window:", err)
+	}
+
+	createLayout(win)
+	win.SetTitle("Simple Example")
+	win.Connect("destroy", func() {
+		gtk.MainQuit()
+	})
+
+	win.SetDefaultSize(800, 600)
+	win.ShowAll()
+
+	gtk.Main()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dossalab/conic-palace
+
+go 1.15
+
+require github.com/gotk3/gotk3 v0.5.1-0.20201111061058-cdb396edc76f
